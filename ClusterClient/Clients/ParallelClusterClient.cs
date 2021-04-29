ee76aba5a8a632fd4e9cf3f6dd839841c109c6c3
MaxCross <dan25001@gmail.com>
2021-04-29 13:13:08 +0500

    Более читаемая версия ParallelClusterClient

--- ClusterClient/Clients/ParallelClusterClient.cs
+++ ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
@@ -20,23 +19,24 @@ namespace ClusterClient.Clients
                 var webRequest = CreateRequest($"{address}?query={query}");
                 return await ProcessRequestAsync(webRequest);
             });
-            var resultTask = WhenAnyCompleted(tasks.ToHashSet());
+            var resultTask = WhenAnyCompleted(tasks);
             await Task.WhenAny(resultTask, Task.Delay(timeout));
 
             if (!resultTask.IsCompleted) throw new TimeoutException();
             return resultTask.Result.Result;
         }
 
-        protected async Task<Task<T>> WhenAnyCompleted<T>(HashSet<Task<T>> tasks)
+        private static async Task<Task<T>> WhenAnyCompleted<T>(IEnumerable<Task<T>> tasks)
         {
-            while (tasks.Any())
+            var tasksCollection = tasks.ToHashSet();
+            while (tasksCollection.Any())
             {
-                var completed = await Task.WhenAny(tasks);
-                if (completed.Status == TaskStatus.RanToCompletion) return completed;
-                tasks.Remove(completed);
+                var completed = await Task.WhenAny(tasksCollection);
+                if (!completed.IsFaulted) return completed;
+                tasksCollection.Remove(completed);
             }
 
-            throw new Exception("Nothing was completed, and I don't know how to fault task properly");
+            throw new Exception("All tasks faulted");
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
